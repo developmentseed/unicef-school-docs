4cbdbd91fddeaf40c3f55292b937e90665a4f14a
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2019-04-03 14:04:11 -0400

    add site url

--- _config.js
+++ _config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  baseurl: 'http://devseed.com/doc-seed',
+  baseurl: 'http://devseed.com/project-connect-docs',
   title: 'Project Connect',
   description: 'AI-assisted school mapping',
 
