68451650eeed1320997b1ff5283cb00a8dd6003d
Zhuangfang Yi <geospatialanalystyi@gmail.com>
2019-04-03 17:13:57 -0400

    post indext

--- _config.js
+++ _config.js
@@ -13,7 +13,7 @@ module.exports = {
   sectionsMeta: {
     's0-introduction': 'Introduction',
     's1-methodology': 'Methodology',
-    's2-results_and_discussion': 'Results_and_Discussion',
+    's2-results_and_discussion': 'Results',
     's3-conclusion': 'Conclusion'
   }
 };
